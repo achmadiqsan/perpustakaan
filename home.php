48d34e65540ba3ae6e9f5d82da1db66bf89aab38
achmadiqsan <achmad.iqsan@gmail.com>
2020-10-25 20:24:17 +0800

    perpustakaan

---
+++ home.php
@@ -0,0 +1,5 @@
+<p></p>
+<body>
+    <center><font size="+3" face="arial">Sistem Informasi Perpustakaan</font></center>
+    <center><font size="+3" face="arial">SD Negeri Parang Tambung I Makassar</font></center>
+</body>
